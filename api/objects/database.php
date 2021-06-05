21205688ce3c13c0bf7943540dbef33621180739
machaa <machaa@trydev.com>
2021-06-05 13:23:58 +0300

    api files

---
+++ api/objects/database.php
@@ -0,0 +1,113 @@
+<?php
+class Database{
+    private $database;
+	private $conn;
+
+	function __construct(){
+        $this->database=DB_NAME;
+        $pass=DB_PASS;
+        $user=DB_USER;
+        $host=DB_HOST;
+        $port=DB_PORT;
+
+        //Connecting to MongoDB
+        if(empty($user && empty($pass))){
+            $uri="mongodb://$host:$port/".$this->database;
+        }
+        if(!empty($user) && empty($pass)){
+            $uri="mongodb://$user@$host:$port/".$this->database;
+        }
+        if(!empty($user) && !empty($pass)){
+            $uri="mongodb://$user:$pass@$host:$port/".$this->database;
+        }
+        try {
+			//Establish database connection
+            $this->conn = new MongoDB\Driver\Manager($uri);
+        }catch (MongoDBDriverExceptionException $e) {
+            echo $e->getMessage();
+			echo nl2br("n");
+        }
+    }
+
+	function getConnection() {
+		return $this->conn;
+	}
+
+    function createRecord($collection_name,$fields){
+        //this function will be called multiple number of times to insert records according to the collection name specified and the fileds given
+        $InsertRecord = new MongoDB\Driver\BulkWrite;
+        $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 100);
+        $InsertRecord->insert($fields);
+        try {
+            $result=$this->conn->executeBulkWrite($this->database.'.'.$collection_name, $InsertRecord,$writeConcern);
+        } catch (MongoDB\Driver\Exception\BulkWriteException $e) {
+            $result = $e->getWriteResult();
+            // Check if any write operations did not complete at all
+            foreach ($result->getWriteErrors() as $writeError) {
+                printf("Operation#%d: %s (%d)\n",
+                $writeError->getIndex(),
+                $writeError->getMessage(),
+                $writeError->getCode());
+            }
+        }
+        if($result->getInsertedCount()==1)
+        {
+            return TRUE;
+        }else
+        {
+            return FALSE;
+        }
+    }
+
+    function deleteRecord($collection_name,$field,$option){
+        //this function deletes a record in the db according to the field
+        $DeleteRecord = new MongoDB\Driver\BulkWrite;
+        $DeleteRecord->delete($field,$option);
+        $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 100);
+        $result = $this->conn->executeBulkWrite($this->database.'.'.$collection_name,$DeleteRecord,$writeConcern);
+        if($result->getDeletedCount()>0)
+        {
+            return TRUE;
+        }else
+        {
+            return FALSE;
+        }
+    }
+    
+    function queryData($collection_name,$filter,$option){
+        //this function executes a query and returns the result as an array
+        $read = new MongoDB\Driver\Query($filter, $option);
+        $records = $this->conn->executeQuery($this->database.'.'.$collection_name, $read);
+        return $records->toArray();
+    }
+    
+    function updateOne($collection_name,$filter,$values){
+        //this function changes a database record to the values given
+        $update = new MongoDB\Driver\BulkWrite;
+        $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 100);
+        $update->update($filter,$values);
+        $result = $this->conn->executeBulkWrite($this->database.'.'.$collection_name, $update,['multi'=>false,'upsert'=>false]);
+        if($result->getModifiedCount() == 1){
+            return TRUE;
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+    function updateMany($collection_name,$filter,$values){
+        //this function changes a database record to the values given
+        $update = new MongoDB\Driver\BulkWrite;
+        $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 100);
+        $update->update($filter,$values);
+        $result = $this->conn->executeBulkWrite($this->database.'.'.$collection_name, $update,['multi'=>true,'upsert'=>false]);
+        if($result->getModifiedCount() > 0){
+            return TRUE;
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+}
+?>
---
+++ api/objects/user.php
@@ -0,0 +1,278 @@
+<?php
+class User{
+  
+    // database connection and table name
+    private $database;
+    private $collection_name;
+  
+    // object properties
+    public $id;
+    public $surname;
+    public $first_name;
+    public $middle_name;
+    public $gender;
+    public $DOB;
+    public $id_no;
+    public $email;
+    public $password;
+    public $password_hash;
+    public $marital_status;
+    public $phone_number;
+    public $join_date;
+    public $county;
+    public $sub_county;
+    public $ward;
+    public $location;
+    public $profile_pic;
+    public $isadmin;
+    public $token;
+    //
+    public $cash;
+  
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->database = $db;
+        $this->collection_name="users";
+        $this->is_admin = false;
+    }
+    //function to sanitize data to prevent unwanted injections
+    public function sanitize($text){
+        return htmlspecialchars(strip_tags($text));
+    }
+
+    public function readUsers(){
+        // read all user records
+        $filter = [];
+        $option = [];
+        $records = $this->database->queryData($this->collection_name,$filter,$option);
+        return $records;
+    }
+
+    public function getUserByEmail(){
+        //check if a user exists through an email and returns all user fields if user exists, or false otherwise
+        $this->email=$this->sanitize($this->email);
+        $filter = ['email'=>$this->email];
+        $option = [];
+        $all_users=$this->database->queryData($this->collection_name,$filter,$option);
+        if(count($all_users)==1){
+            return $all_users;
+        }else{
+            return FALSE;
+        }
+    }
+    public function getUserById(){
+        //this function gets a user by id and returns all the values
+        $filter=["_id"=>new MongoDB\BSON\ObjectId($this->sanitize($this->id))];
+        $option = [];
+        $values=$this->database->queryData($this->collection_name,$filter,$option);
+        if($values){
+            return $values;
+        }
+        else{
+            return false;
+        }
+    }
+    public function checkLogin(){
+        //set the password from the database that a user requires to login and return true if user exists
+        $data = $this->getUserByEmail();
+        if($data){
+            foreach($data as $row){
+                $this->password = $row->password;
+                $this->id = $row->_id;
+                $this->isadmin;
+            }
+            return TRUE;
+
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
+    public function createUser(){
+        //function to create a user in the database
+        //hash password with bcrypt before storing
+        $this->password_hash=password_hash($this->sanitize($this->password), PASSWORD_BCRYPT);
+        $values = array();
+        $values=[
+            'surname' =>$this->sanitize($this->surname),
+            'first_name' =>$this->sanitize($this->first_name),
+            'middle_name' =>$this->sanitize($this->middle_name),
+            'gender' =>$this->sanitize($this->gender),
+            'DOB' =>$this->sanitize($this->DOB),
+            'id_no' =>$this->sanitize($this->id_no),
+            'email'=>$this->sanitize($this->email),
+            'password'=>$this->password_hash,
+            'marital_status'=>$this->sanitize($this->marital_status),
+            'phone_number'=>$this->sanitize($this->phone_number),
+            'join_date'=>$this->sanitize($this->join_date),
+            'county'=>$this->sanitize($this->county),
+            'sub_county'=>$this->sanitize($this->sub_county),
+            'ward'=>$this->sanitize($this->ward),
+            'location'=>$this->sanitize($this->location),
+            'profile_pic'=>$this->sanitize($this->profile_pic),
+            'isadmin'=>$this->isadmin
+        ];
+        if($this->database->createRecord($this->collection_name,$values)){
+            return TRUE;
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+    public function signUp(){
+        //function to create a user in the database
+        //hash password with bcrypt before storing
+        $this->password_hash=password_hash($this->sanitize($this->password), PASSWORD_BCRYPT);
+        $values = array();
+        $values=[
+            'first_name' =>$this->sanitize($this->first_name),
+            'middle_name' =>$this->sanitize($this->middle_name),
+            'id_no' =>$this->sanitize($this->id_no),
+            'phone_number'=>$this->sanitize($this->phone_number),
+            'email'=>$this->sanitize($this->email),
+            'password'=>$this->password_hash,
+            'join_date'=>$this->sanitize($this->join_date),
+            'isadmin'=>$this->isadmin
+        ];
+        if($this->database->createRecord($this->collection_name,$values)){
+            return TRUE;
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
+
+    public function deleteUser(){
+        //sanitize id input from user
+        $this->id=$this->sanitize($this->id);
+        $filter = ['_id'=>new MongoDB\BSON\ObjectId($this->id)];
+        $option = ['limit'=>1];
+        if($this->database->deleteRecord($this->collection_name,$filter,$option)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function setToken(){
+        //this function sets a token for password recorvery
+        $this->email = $this->sanitize($this->email);
+        $filter = ['email'=>$this->email];
+        $option = ['$set'=>['token' =>$this->token]];
+        if($this->database->updateOne($this->collection_name,$filter,$option))
+        {
+            return TRUE;
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
+    public function resetPassword(){
+        ///this function changes a user password
+        //check if user exists in db through the email and if token is valid and does the change
+        $data = $this->getUserByEmail();
+        if($data){
+            foreach($data as $row){
+                $true_token=$row->token;
+                $this->id = $row->_id;
+            }
+            //check if token matches in DB
+            if($this->token === $true_token ){
+                $filter = ['_id'=>new MongoDB\BSON\ObjectID($this->id)];
+                //hash password for storage
+                $this->password_hash=password_hash($this->sanitize($this->password), PASSWORD_BCRYPT);
+                $this->token="";
+                $option = ['$set'=>['password'=>$this->password_hash,'token'=>$this->token]];
+                if($this->database->updateOne($this->collection_name,$filter,$option))
+                {
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+    }
+    public function setUserProfile(){
+        //this function when called sets the users field in the db
+        $data = $this->getUserByEmail();
+        if($data){
+            foreach($data as $row){
+                $this->id = $row->_id;
+                $this->surname = $row->surname;
+                $this->first_name  = $row->first_name;
+                $this->middle_name  = $row->middle_name;
+                $this->gender  = $row->gender;
+                $this->DOB  = $row->DOB;
+                $this->id_no  = $row->id_no;
+                $this->email = $row->email;
+                $this->password_hash = $row->password;
+                $this->marital_status = $row->marital_status;
+                $this->phone_number = $row->phone_number;
+                $this->join_date = $row->join_date;
+                $this->county = $row->county;
+                $this->sub_county = $row->sub_county;
+                $this->ward = $row->ward;
+                $this->location = $row->location;
+                $this->profile_pic = $row->profile_pic;
+            }
+        }
+    }
+    public function UpdateUserProfile(){
+        $this->email = $this->sanitize($this->email);
+        $filter = ['email'=>$this->email];
+        if(!empty($this->password)){
+            $this->password_hash=password_hash($this->sanitize($this->password), PASSWORD_BCRYPT);
+        }
+        $values = array();
+        $values=['$set'=>[
+            'surname' =>$this->sanitize($this->surname),
+            'first_name' =>$this->sanitize($this->first_name),
+            'middle_name' =>$this->sanitize($this->middle_name),
+            'gender' =>$this->sanitize($this->gender),
+            'DOB' =>$this->sanitize($this->DOB),
+            'id_no' =>$this->sanitize($this->id_no),
+            'email'=>$this->sanitize($this->email),
+            'password'=>$this->password_hash,
+            'marital_status'=>$this->sanitize($this->marital_status),
+            'phone_number'=>$this->sanitize($this->phone_number),
+            'join_date'=>$this->sanitize($this->join_date),
+            'county'=>$this->sanitize($this->county),
+            'sub_county'=>$this->sanitize($this->sub_county),
+            'ward'=>$this->sanitize($this->ward),
+            'location'=>$this->sanitize($this->location),
+            'profile_pic'=>$this->sanitize($this->profile_pic),
+            'isadmin'=>$this->isadmin
+            
+        ]];
+         if($this->database->updateOne($this->collection_name,$filter,$values))
+        {
+            return TRUE;
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+    public function getCash(){
+        $this->id=$this->sanitize($this->id);
+        $filter=["_id"=>new MongoDB\BSON\ObjectId($this->sanitize($this->id))];
+        $option=[];
+        $values=$this->database->queryData($this->collection_name,$filter,$option);
+        foreach($values as $row){
+            $this->cash=$row->cash;
+        }
+            return $this->cash;
+
+    }
+    
+}
+?>
