d0c2d2bd297d8e827ef09a87754819afd095c176
machaa <machaa@trydev.com>
2021-06-07 11:54:22 +0300

    added interest config

--- config/config.php
+++ config/config.php
@@ -7,9 +7,9 @@ define('ROOT', __DIR__ .'/../');
 //database is mongo this section includes the mongo config files
 define('DB_HOST','localhost'); //specify the host here
 define('DB_PORT',27017);//specify the port  here
-define('DB_NAME','');//specify the dbname here cant be blank
-define('DB_USER','');//specify the user here leave blank if none
-define('DB_PASS','');//specify the user password here leave blank if none
+define('DB_NAME','chama');//specify the dbname here cant be blank
+define('DB_USER','chama');//specify the user here leave blank if none
+define('DB_PASS','chama');//specify the user password here leave blank if none
 //end mongo config
 
 //jwt config is in this section
@@ -24,13 +24,16 @@ $expiration=$issued_at+(60*60); //validity for tokens i.e valid for one hour
 define('EMAIL_HOST','smtp.gmail.com'); //email host required for sending reset token
 define('EMAIL_PORT',587); //the email host port
 define('EMAIL_ADDRESS','');//email address to use for sending mails
-define('EMAIL_PASS','');//email password
+define('EMAIL_PASS',' ');//email password
 define('EMAIL_NAME',''); //email name to include while sending mails
 define('EMAIL_DEBUG',0);//whether debug is turned on
 //end email config
 
 //fix cross origin errors
 //enter url name
-define('URL','http://localhost')
+define('URL','http://localhost');
 //end url name
+
+//the interest rate for all loans is defined here
+define('INTEREST',20);
 ?>
