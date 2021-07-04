cdad2ddef443f8fcabbcd5d657a7dcd7c7e5f01c
machaa <machaa@trydev.com>
2021-07-04 20:48:16 +0300

    modified

--- api/user/getcash.php
+++ api/user/getcash.php
@@ -4,6 +4,7 @@ include_once '../../config/config.php';
 //include the user and database objects
 include(ROOT.'api/objects/database.php');
 include(ROOT.'api/objects/user.php');
+include(ROOT.'api/objects/accounts.php');
 include(ROOT.'api/objects/auth.php');
 
 //include the required headers
@@ -15,17 +16,18 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 //instantiate objects
 $database  = new Database();
-$user = new User($database);
+$user = new Loan($database);
+
 $auth = new Auth();
-$data = json_decode(file_get_contents("php://input"));
-if($_SERVER['HTTP_AUTHORIZATION']){
-    $autharray=explode(" ",$_SERVER['HTTP_AUTHORIZATION']);
+if($_SERVER["HTTP_AUTHORIZATION"]){
+    $autharray=explode(" ",$_SERVER["HTTP_AUTHORIZATION"]);
     $jwt=$autharray[1];
     $decoded=$auth->Decode($jwt);
     if($decoded['valid']){
         $data=$decoded['data']->data;
         $user->email=$data->email;
         $user->setUserProfile();
+        $user->setAccountNo();
         echo json_encode($user->getAccountBal());
 
     }else{
